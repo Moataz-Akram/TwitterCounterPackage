b552177e5ba3d22cb115da4282a38520f13c2942
Moataz-Akram <moataz.akram@vodafone.com>
2022-09-26 01:21:17 +0200

    add path: "Sources" to dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TwitterCounterPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "TwitterCounterPackageTests",
             dependencies: ["TwitterCounterPackage"]),
