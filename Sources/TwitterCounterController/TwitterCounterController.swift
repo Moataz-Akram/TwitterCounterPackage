42c97b9a6ad7530c5f6240d4c98ac249ea1f0cec
Moataz-Akram <moataz.akram@vodafone.com>
2022-09-26 01:41:08 +0200

    fix controller init

--- Sources/TwitterCounterController/TwitterCounterController.swift
+++ Sources/TwitterCounterController/TwitterCounterController.swift
@@ -12,5 +12,9 @@ public class TwitterCounterController: UIViewController {
     public init(){
         super.init(nibName: "TwitterCounterController", bundle: Bundle.module)
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
